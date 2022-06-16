aade023474bc6e0f27d723670767130b678b7113
ROHIT DOYAL <rohit.2@iitj.ac.in>
2022-06-16 07:10:36 +0530

    lis concept added

--- dp/LIS/lis.cpp
+++ dp/LIS/lis.cpp
@@ -41,13 +41,7 @@ int main(){
     //index = 0->n-1 = n element 
     //prev =  -1->n-1 = n+1 element 
      int n = arr.size();
-    // dp=  new int*[n];
-    // for (int i = 0; i < n; i++)
-    // {
-    //     /* code */
-    //     dp[i] =  new int[n+1];
-        
-    // }
+    
 
     for(int i =0; i<n; i++){
         for (int j = 0; j< n+1; j++)
