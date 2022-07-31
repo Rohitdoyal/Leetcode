cf48faaf26e58fa3f638f7c49d53ffb004952e6e
ROHIT DOYAL <rohit.2@iitj.ac.in>
2022-07-31 20:03:30 +0530

    frog question added

--- dp/atcoder/frog1.cpp
+++ dp/atcoder/frog1.cpp
@@ -1,7 +1,32 @@
 #include<bits/stdc++.h> 
 using namespace std;
 
+int dp[100001];
+int f(int i ,int* cost, int n){
+
+    if(i>=n-1){
+        return 0;
+    }
+   
+    if(dp[i]!=-1){return dp[i];}
+
+    else{
+        return dp[i] =min(abs(cost[i]-cost[i+1])+f(i+1,cost,n),abs(cost[i]-cost[i+2])+f(i+2,cost,n));
+    }
+}
+
 int main(){
+    long long int n ;
+    cin>>n;
+
+    int v[n];
+    for (int i = 0; i <n; i++)
+    {
+        cin>>v[i];
+    }
+ 
+    memset(dp, -1, sizeof(dp));
+    cout<<f(0,v,n)<<endl;
     
     return 0;
 }
