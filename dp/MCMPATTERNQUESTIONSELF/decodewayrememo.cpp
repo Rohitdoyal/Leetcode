1f588dec6f26c65845f6df455199b5e946a0168e
ROHIT DOYAL <rohit.2@iitj.ac.in>
2022-08-23 23:27:56 +0530

    question added

---
+++ dp/MCMPATTERNQUESTIONSELF/decodewayrememo.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+class Solution {
+public:
+    
+    
+    int f(int i, string s,vector<int>&dp){
+        if(i==s.length()){
+            return 1;
+        }
+        
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        
+        int count =0;
+        if(s[i]!='0'){
+            count += f(i+1,s,dp);
+        }
+        
+        if((i+1<s.length()) && (s[i]=='1'||(s[i]=='2') && (s[i+1]<='6'))){
+                count += f(i+2,s,dp);
+        }
+        return dp[i]=count;
+        
+    }
+    
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int>dp(n,-1);
+        return f(0,s,dp);
+    }
+};
+int main(){
+    
+    return 0;
+}
