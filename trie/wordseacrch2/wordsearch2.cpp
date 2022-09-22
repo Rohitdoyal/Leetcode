4bfdac6e663050984cfa766488a22880b045d995
Rohitdoyal <rohit.2@iitj.ac.in>
2022-09-22 18:17:34 +0530

    word search 2 question added

--- trie/wordseacrch2/wordsearch2.cpp
+++ trie/wordseacrch2/wordsearch2.cpp
@@ -1,40 +1,75 @@
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
-
-
- bool dfs(vector<vector<char>>& A ,int i,int j,string word,int idx){
-        if((i<0) || (i>=A.size()) || (j<0) || (j>=A[0].size()) || (word[idx]!=A[i][j])){
-             return false;
+class Solution {
+public:
+    
+    struct trinode{
+        string word="";
+        struct trinode* children[26]={NULL};
+    };
+    
+    
+    struct trinode* buildtree(vector<string> &words){
+        struct trinode* root = new trinode();
+        for(int j =0; j<words.size(); j++){
+            string word= words[j];
+            struct trinode* curr =root ;
+            for(int i =0; i<word.length(); i++){
+                int index = word[i]-'a';
+                if(curr->children[index]==NULL){
+                    curr->children[index] = new trinode();
+                }
+                curr = curr->children[index];
+            }
+            curr->word = word;
         }
-       
-        if(idx==word.length()-1){
-            return true;
+        return root;
+    }
+    
+    void dfs(int i , int j ,vector<vector<char>>& board ,struct trinode* root, vector<string>&res){
+        
+        char c= board[i][j];
+        if(c=='#' || (root->children[c-'a']==NULL) ){return;}
+        
+        int n = board.size();
+        int m = board[i].size();
+        
+        root= root->children[c-'a'];
+        if((root->word).size()>0){
+            res.push_back(root->word);
+            root->word = "";
+        }
+        //do
+        board[i][j] ='#';
+        
+            
+        //recure
+        if(i+1<n){dfs(i+1,j,board,root,res);}
+        if(i-1>=0){dfs(i-1,j,board,root,res);}
+        if(j+1<m){dfs(i,j+1,board,root,res);}
+        if(j-1>=0){dfs(i,j-1,board,root,res);}
+        
+        //undo 
+        board[i][j] = c;
+        return;
+        
+        
+    }
+    
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        
+        struct trinode*root = buildtree(words);
+        vector<string> res;
+        for(int i = 0; i<board.size(); i++){
+            for(int j =0; j<board[i].size(); j++){
+                dfs(i,j,board,root,res);
+            }
         }
         
-        char c=A[i][j];
-        A[i][j]='#';
-        bool res=dfs(A,i+1,j,word,idx+1) ||
-        dfs(A,i-1,j,word,idx+1) ||
-        dfs(A,i,j+1,word,idx+1) ||
-        dfs(A,i,j-1,word,idx+1);
-        A[i][j]=c;
         return res;
     }
-    
-    
-    bool exist(vector<vector<char>>& board, string word) {
-        
-        
-        for(int i =0; i<board.size(); i++){
-            for(int j =0; j<board[i].size(); j++){
-                if(board[i][j]==word[0] && dfs(board,i,j,word,0)==true){
-                        return true;
-                    }
-                }
-            }
-        
-        return false;
-    }
+};
+
 int main(){
     
     return 0;
